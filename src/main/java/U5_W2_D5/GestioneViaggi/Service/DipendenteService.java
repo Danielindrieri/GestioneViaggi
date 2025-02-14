da78e4b67c1c8716bab51e5cb1f882c22ec86061
Daniel Indrieri <indrieridaniel@gmail.com>
2025-02-14 12:35:10 +0100

    creazione dei controller

--- src/main/java/U5_W2_D5/GestioneViaggi/Service/DipendenteService.java
+++ src/main/java/U5_W2_D5/GestioneViaggi/Service/DipendenteService.java
@@ -2,11 +2,9 @@ package U5_W2_D5.GestioneViaggi.Service;
 
 import U5_W2_D5.GestioneViaggi.Repository.DipendenteRepository;
 import U5_W2_D5.GestioneViaggi.entities.Dipendente;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.Valid;
+import U5_W2_D5.GestioneViaggi.entities.Viaggio;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.mapper.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,31 +12,37 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-
-
 @Service
 @Validated
 @RequiredArgsConstructor
 public class DipendenteService {
     private final DipendenteRepository repository;
-    private final Dipendente;
-    @Autowired
 
+    // Metodo per salvare un nuovo Dipendente
     public Dipendente save(Dipendente body) {
-        DipendenteRepository.findByEmail(body.getEmail()).ifPresent(user -> {
-            throw new BadRequestException("L'email " + body.getEmail() + " è già stata utilizzata");
+        // Controllo se l'email è già registrata
+        repository.findByEmail(body.getEmail()).ifPresent(user -> {
+            throw new IllegalArgumentException("L'email " + body.getEmail() + " è già stata utilizzata");
         });
-        body.setAvatar("https://ui-avatars.com/api/?name=" + body.getName().charAt(0) + "+" + body.getSurname().charAt(0));
-        return DipendenteRepository.save(body);
+
+        // Creazione avatar
+        if (body.getName() != null && body.getUsername() != null) {
+            body.setAvatar("https://ui-avatars.com/api/?name=" +
+                    body.getName().charAt(0) + "+" + body.getUsername().charAt(0));
+        }
+
+        // Salvataggio del Dipendente
+        return repository.save(body);
     }
 
-    public Page<Author> getAuthors(int page, int size, String sort) {
-
+    // Metodo per ottenere una lista paginata di Dipendenti
+    public Page<Viaggio> getDipendenti(int page, int size, String sort) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
-        return authorsRepository.findAll(pageable);
+        return repository.findAll(pageable);
     }
 
-    public Author findById(int id) {
-        return authorsRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    // Metodo per trovare un Dipendente per ID
+    public Dipendente  findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new NotFoundException());
     }
 }
