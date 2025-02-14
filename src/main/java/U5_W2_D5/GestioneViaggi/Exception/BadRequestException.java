2c22265512a824a4fc54d9c59affd67f3d42563a
Daniel Indrieri <indrieridaniel@gmail.com>
2025-02-14 14:37:59 +0100

    creaziione exception

---
+++ src/main/java/U5_W2_D5/GestioneViaggi/Exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package U5_W2_D5.GestioneViaggi.Exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
