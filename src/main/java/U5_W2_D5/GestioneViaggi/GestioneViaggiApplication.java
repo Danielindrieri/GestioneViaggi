83198bb4f4ac35b7386754e734004247e1b86012
Daniel Indrieri <indrieridaniel@gmail.com>
2025-02-14 11:07:18 +0100

    Initial commit

---
+++ src/main/java/U5_W2_D5/GestioneViaggi/GestioneViaggiApplication.java
@@ -0,0 +1,13 @@
+package U5_W2_D5.GestioneViaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneViaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/U5_W2_D5/GestioneViaggi/GestioneViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package U5_W2_D5.GestioneViaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
